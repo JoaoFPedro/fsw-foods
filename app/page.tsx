3a4d89a80113db06c85d49cea12c115120532ba5
Joao Pedro <joao.fpsilva98@gmail.com>
2024-06-13 18:01:18 -0300

    feat: Search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+
 import Header from "./_components/Header";
+import Search from "./_components/Search";
 
 const Home = () => {
   return ( 
+    <>
     <Header />
+    <div className="px-5 pt-6">
+    <Search />
+    </div>
+    </>
    );
 }
  
