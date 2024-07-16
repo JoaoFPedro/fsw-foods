c90536fb538019e808a920f6122e27d63817d0a5
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-16 18:49:27 -0300

    fix: adding padding botton to category-list component.

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -6,7 +6,7 @@ const CategoryList = async() => {
     const categories = await db.category.findMany({})
     return ( 
       <>
-      <div className="grid grid-cols-2 gap-3">
+      <div className="grid grid-cols-2 gap-3 pb-4">
         {categories.map(category => <CategoryItem  key={category.id} category={category}/>)}
         </div>
       </>
