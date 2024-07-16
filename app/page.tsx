da9d1ab7e25869dc82420e7e979b0b519c2ce2f2
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-16 18:49:03 -0300

    feat: adding promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 
+import Image from "next/image";
 import Header from "./_components/Header";
 import Search from "./_components/Search";
 import CategoryList from "./_components/category-list";
@@ -14,6 +15,10 @@ const Home = () => {
     <div className="px-5 pt-3">
     <CategoryList />
     </div>
+  
+    <Image  src='/promoBanner01.png' alt="Até 30% de desconto em pizzas" height={0} width={0} className="h-auto w-full object-contain" sizes="100vw" quality={100}/>
+
+    
     </>
    );
 }
