0f9a62bd1792198881bcc6d6eb760086231660ab
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-19 21:24:41 -0300

    feat: adding product price and product discont

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -1,15 +1,16 @@
 import { Prisma, Product } from "@prisma/client";
 import Image from "next/image";
+import { calculatedProductTotalPrice } from "../_helpers/price";
 
 interface ProductItemProps {
-  product: Prisma.ProductGetPayload <{
-    include:{
-      restaurant:{
-        select:{
-          name: true
-        }
-      }
-    }
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
   }>;
 }
 
@@ -25,9 +26,23 @@ const ProductItem = ({ product }: ProductItemProps) => {
         />
       </div>
       <div>
-        <h1>{product.name}</h1>
-        {/* <p>R${product.price}</p> */}
-        <p>{product.restaurant.name}</p>
+        <h2 className="truncate text-sm">{product.name}</h2>
+        <div className="flex gap-2 items-center">
+          <h3 className="font-semibold">
+           R$ {Intl.NumberFormat("pt-BR", {
+              currency: "BRL",
+              minimumFractionDigits: 2,
+            }).format(Number(calculatedProductTotalPrice(product)))}
+          </h3>
+          {product.discountPercentage >0 && (
+            <span className="text-muted-foreground line-through text-xs pt-1 ">R$ {Intl.NumberFormat("pt-BR", {
+              currency: "BRL",
+              minimumFractionDigits: 2,
+            }).format(Number(product.price))}</span>
+          )
+          }
+        </div>
+        <p className="text-muted-foreground">{product.restaurant.name}</p>
       </div>
     </div>
   );
