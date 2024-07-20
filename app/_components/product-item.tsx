a0bf71a181af7bfc5c5e9d13409173ce78d39264
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-19 21:31:13 -0300

    feat: adding format currency function to product-item component

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -1,6 +1,6 @@
 import { Prisma, Product } from "@prisma/client";
 import Image from "next/image";
-import { calculatedProductTotalPrice } from "../_helpers/price";
+import { calculatedProductTotalPrice, formatCurrency } from "../_helpers/price";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -27,20 +27,15 @@ const ProductItem = ({ product }: ProductItemProps) => {
       </div>
       <div>
         <h2 className="truncate text-sm">{product.name}</h2>
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <h3 className="font-semibold">
-           R$ {Intl.NumberFormat("pt-BR", {
-              currency: "BRL",
-              minimumFractionDigits: 2,
-            }).format(Number(calculatedProductTotalPrice(product)))}
+            {formatCurrency(calculatedProductTotalPrice(product))}
           </h3>
-          {product.discountPercentage >0 && (
-            <span className="text-muted-foreground line-through text-xs pt-1 ">R$ {Intl.NumberFormat("pt-BR", {
-              currency: "BRL",
-              minimumFractionDigits: 2,
-            }).format(Number(product.price))}</span>
-          )
-          }
+          {product.discountPercentage > 0 && (
+            <span className="pt-1 text-xs text-muted-foreground line-through">
+              {formatCurrency(Number(product.price))}
+            </span>
+          )}
         </div>
         <p className="text-muted-foreground">{product.restaurant.name}</p>
       </div>
