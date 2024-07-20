ebb6dc2ca8e2a0afd1ad7cf1eb909e8d40bd4dde
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-19 21:24:10 -0300

    exporting function calculateProductTotalPrice

--- app/_helpers/price.ts
+++ app/_helpers/price.ts
@@ -1,11 +1,11 @@
 import { Product } from "@prisma/client";
 
-const calculatedProductTotalPrice = (product: Product) => {
+export const calculatedProductTotalPrice = (product: Product) => {
     if(product.discountPercentage === 0 ){
         return Number(product.price)
     }
 
     const discount = Number(product.price) * (product.discountPercentage/100)
-    
+
     return Number(product.price )- discount
 }
