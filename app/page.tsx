f4916b1c2f4382197ce48e38d6398a49d039dd1f
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-21 13:40:30 -0300

    feat: adding recomendation div and ver todos button

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import { Button } from "./_components/ui/button";
 import { ChevronRight } from "lucide-react";
 import RestaurantList from "./_components/restaurant-list";
 
-
 const Home = () => {
   return (
     <>
@@ -31,24 +30,30 @@ const Home = () => {
       />
       <div className="pt-5">
         <div className="flex items-center justify-between px-5">
-        <h2>Pedidos Recomendados</h2>
-        <Button variant='ghost' className="text-primary p-0">Ver todos
-          <ChevronRight/ >
-        </Button>
+          <h2>Pedidos Recomendados</h2>
+          <Button variant="ghost" className="p-0 text-primary text-xs">
+            Ver todos
+            <ChevronRight />
+          </Button>
         </div>
-      <ProductList />
+        <ProductList />
       </div>
-      
-      
+
       <Image
         src="/promoBanner02.png"
         alt="Até 30% de desconto em pizzas"
         height={0}
         width={0}
-        className="h-auto w-full object-contain pt-4 pb-4"
+        className="h-auto w-full object-contain pb-4 pt-4"
         sizes="100vw"
         quality={100}
       />
+      <div className="flex items-center justify-between px-5">
+        <h2>Restaurantes Recomendados</h2>
+        <Button variant="ghost" className="p-0 text-primary text-xs">
+          Ver todos <ChevronRight />
+        </Button>
+      </div>
       <RestaurantList />
     </>
   );
