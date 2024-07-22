2463fd5910e195d0233035c40a1db4f6d5d1cef0
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-22 19:54:30 -0300

    feat: adding handleBackClick

--- app/products/[id]/_components/product-image.tsx
+++ app/products/[id]/_components/product-image.tsx
@@ -1,11 +1,20 @@
+"use client"
 import { Button } from "@/app/_components/ui/button";
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+
 interface ProductImageProps {
     product: Pick<Product, 'name' | 'imageUrl'>
 }
 const ProductImage = ({product}:ProductImageProps) => {
+    const router = useRouter()
+
+    const handleBackClick =() =>{
+        router.push('/')
+    }
     return ( 
         <>
          <Image
@@ -15,7 +24,7 @@ const ProductImage = ({product}:ProductImageProps) => {
           className="object-cover"
           sizes="icon"
         />
-        <Button
+        <Button onClick={handleBackClick}
           className="hover:tex-white absolute left-4 top-4 rounded-full bg-white text-foreground"
           size="icon"
         >
