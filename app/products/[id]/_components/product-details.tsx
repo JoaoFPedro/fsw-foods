18a831aaf0b3b0f9677132ceaf6748af75cfb618
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-29 19:06:06 -0300

    feat: adding border to product-details component

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -46,7 +46,7 @@ const ProductDetails = ({
     });
   };
   return (
-    <div className="p-5">
+    <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl rounded-tr-3xl bg-white py-5 p-5">
       {/* RESTURANTE */}
       <div className="flex items-center gap-[0.375rem]">
         <div className="relative h-6 w-6">
