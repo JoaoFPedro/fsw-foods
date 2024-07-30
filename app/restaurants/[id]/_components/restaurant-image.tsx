0484c9664eb0d71fe7d8b6e630131eca3a6eb166
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-30 20:53:31 -0300

    feat: adding handleBackClick function

--- app/restaurants/[id]/_components/restaurant-image.tsx
+++ app/restaurants/[id]/_components/restaurant-image.tsx
@@ -1,11 +1,23 @@
+"use client"
+import { Button } from "@/app/_components/ui/button";
 import { Restaurant } from "@prisma/client";
+import { ChevronLeftIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 interface RestaurantImageProps {
     restaurants: Pick<Restaurant, 'name' | 'imageUrl'>
 }
 
 const RestaurantImage = ({restaurants}: RestaurantImageProps) => {
-    return ( 
+    const navigation = useRouter()
+
+    const handleBackClick = () => {
+        navigation.back()
+    }
+    return (
+        
+        //  IMAGE 
         <div className="relative h-[360px] w-full">
         <Image
           src={restaurants?.imageUrl}
@@ -13,6 +25,11 @@ const RestaurantImage = ({restaurants}: RestaurantImageProps) => {
           fill
           className="object-cover"
         />
+        {/* LEFT BUTTON */}
+        <Button className="absolute bg-white top-2 left-2 text-foreground" size='icon' onClick={handleBackClick}>
+            <ChevronLeftIcon />
+
+        </Button>
       </div>
      );
 }
