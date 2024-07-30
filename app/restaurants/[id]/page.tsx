24a179b819589aa80e2db390535a8326a7199aa0
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-30 20:08:48 -0300

    feat: initializing restaurant page

---
+++ app/restaurants/[id]/page.tsx
@@ -0,0 +1,13 @@
+interface RestaurantPageProps {
+    params: {
+        id: string
+    }
+}
+
+const RestaurantPage = ({params:{id}}: RestaurantPageProps) => {
+    return ( 
+        <h1>Restaurant Page {id}</h1>
+     );
+}
+ 
+export default RestaurantPage;
