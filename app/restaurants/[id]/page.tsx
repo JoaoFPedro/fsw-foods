5230964838f83b586f773709924e9ce666a4b64f
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-30 20:18:57 -0300

    feat: add restaurant image

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -1,13 +1,33 @@
+import { db } from "@/app/_lib/prisma";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 interface RestaurantPageProps {
-    params: {
-        id: string
-    }
+  params: {
+    id: string;
+  };
 }
 
-const RestaurantPage = ({params:{id}}: RestaurantPageProps) => {
-    return ( 
-        <h1>Restaurant Page {id}</h1>
-     );
-}
- 
+const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
+  const restaurants = await db.restaurant.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!restaurants) {
+    return notFound();
+  }
+  return (
+    <div>
+      <Image
+        src={restaurants?.imageUrl}
+        alt={restaurants?.name}
+        fill
+        className="object-cover"
+      />
+    </div>
+  );
+};
+
 export default RestaurantPage;
