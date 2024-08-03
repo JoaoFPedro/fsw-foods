c26c35a01cc42948568f54d66ccc11be36085b54
Joao Pedro <joao.fpsilva98@gmail.com>
2024-08-03 19:25:28 -0300

    feat: Initialize category page

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -1,21 +1,29 @@
+"use client"
 import { Category } from "@prisma/client";
 import Image from "next/image";
+
 
 interface CategoryItemProps {
   category: Category;
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
+  const handleClick = () => {
+    console.log('teste')
+}
   return (
-    <div className="flex items-center gap-3 rounded-full px-4 py-3 shadow-md">
-      <Image
-        src={category.imageUrl}
-        alt={category.name}
-        height={30}
-        width={30}
-      />
-      <h1>{category.name}</h1>
-    </div>
+    
+           
+    <div className="flex items-center gap-3 rounded-full px-4 py-3 shadow-md" onClick={handleClick}>
+    <Image
+    src={category.imageUrl}
+    alt={category.name}
+    height={30}
+    width={30}
+  />
+  <h1>{category.name}</h1>
+  </div>
+    
   );
 };
 
