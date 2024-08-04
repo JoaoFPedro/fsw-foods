ab4e5a32560f23f3f7aecc33333f2a2002de9099
Joao Pedro <joao.fpsilva98@gmail.com>
2024-08-04 19:03:21 -0300

    initializing menu sidebar

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,16 +1,51 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Input } from "postcss";
 
 const Header = () => {
-    return ( 
-        <div className="flex justify-between px-5 pt-6">
-            <Image src="/Logo.png" alt="FSW Foods" height={30} width={100} />
-            <Button size="icon" variant="outline" className="border-none bg-transparent">
-                <MenuIcon />
-            </Button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="flex justify-between px-5 pt-6">
+
+
+      <Image src="/Logo.png" alt="FSW Foods" height={30} width={100} />
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            size="icon"
+            variant="outline"
+            className="border-none bg-transparent"
+          >
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <SheetDescription>
+              Make changes to your profile here. Click save when youre done.
+            </SheetDescription>
+          </SheetHeader>
+          
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Save changes</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+
 export default Header;
