9878963abc5926e4af1cad39f1932217dedbe593
Joao Pedro <joao.fpsilva98@gmail.com>
2024-08-04 12:24:46 -0300

    feat: rendering product item at CategoryItemPage

--- app/category/[id]/page.tsx
+++ app/category/[id]/page.tsx
@@ -1,23 +1,47 @@
 import { db } from "@/app/_lib/prisma";
 import Image from "next/image";
+import CategoryItemList from "./_componentes/categoryItem-list";
+import { notFound } from "next/navigation";
+import ProductItem from "@/app/_components/product-item";
 
 interface CategoryPageProps {
-    params :{
-        id: string
-    }
+  params: {
+    id: string;
+   
+  };
 }
-const CategoryPage = async ({params: {id}}:CategoryPageProps) => {
-    const category = await db.product.findMany({
-where: {
-    category: {
-        name:'Sucos'
-    }
-}
-    })
-    console.log('category',category)
-    return ( 
-        <Image src={category[0].imageUrl} fill alt={category[0].name} />
-     );
-}
+const CategoryPage = async ({
+  params: { id },
+}: CategoryPageProps) => {
+  console.log(id);
+  const category = await db.category.findUnique({
+    where: {
+        id,
+      },
+      include: {
+        products:{
+            include: {
+                restaurant: {
+                    select: {
+                        name: true
+                    }
+                }
+            }
+        }
+      }
+    });
+  
  
+  if (!category) {
+    return notFound();
+  }
+  return (
+    <div>
+    {category.products.map((product) => (
+        <ProductItem key={product.id} product={product}/>
+    ))}
+    </div>
+  );
+};
+
 export default CategoryPage;
