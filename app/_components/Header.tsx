5743b64997cbcda9a4412a587d4f28d5fa42ea77
Joao Pedro <joao.fpsilva98@gmail.com>
2024-08-04 20:17:22 -0300

    feat: adding login icon and menu's icon

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import {
+  HeartIcon,
+  HomeIcon,
+  LogInIcon,
+  MenuIcon,
+  UtensilsIcon,
+} from "lucide-react";
 import {
   Sheet,
   SheetClose,
@@ -16,8 +22,6 @@ import { Input } from "postcss";
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-
-
       <Image src="/Logo.png" alt="FSW Foods" height={30} width={100} />
       <Sheet>
         <SheetTrigger asChild>
@@ -32,15 +36,29 @@ const Header = () => {
         <SheetContent>
           <SheetHeader>
             <SheetTitle>Menu</SheetTitle>
-            <SheetDescription>
-              Make changes to your profile here. Click save when youre done.
-            </SheetDescription>
           </SheetHeader>
-          
+          <div className="mb-4 flex justify-between border-b-2 border-blue-500 pb-3">
+            <span className="">Olá, Faça seu login!</span>
+            <div>
+              <LogInIcon className="text-xl" />
+            </div>
+          </div>
+          <div className="mb-4 w-full flex items-center space-x-2 rounded-full px-4  text-sm hover:bg-red-600 hover:text-white">
+            <HomeIcon size={16} />
+            <span>Inicio</span>
+          </div>
+          <div className="mb-4 w-full flex items-center space-x-2 rounded-full px-4  text-sm hover:bg-red-600 hover:text-white">
+            <UtensilsIcon size={16}/>
+            <span>Meus Pedidos</span>
+          </div>
+          <div className="mb-4 w-full flex items-center space-x-2 rounded-full px-4 text-sm hover:bg-red-600 hover:text-white">
+            <HeartIcon size={16}/>
+            <span>Restaurantes Favoritos</span>
+          </div>
           <SheetFooter>
-            <SheetClose asChild>
+            {/* <SheetClose asChild>
               <Button type="submit">Save changes</Button>
-            </SheetClose>
+            </SheetClose> */}
           </SheetFooter>
         </SheetContent>
       </Sheet>
