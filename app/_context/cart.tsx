d00ea2a060ca00376f3c68d5a044bc68b37cfe25
Joao Pedro <joao.fpsilva98@gmail.com>
2024-08-18 12:02:41 -0300

    feat: cartContext

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -1,5 +1,6 @@
+"use client"
 import { Product } from "@prisma/client"
-import { createContext } from "react"
+import { createContext, ReactNode, useState } from "react"
 
 interface CartProduts extends Product {
     quantity: number
@@ -7,8 +8,24 @@ interface CartProduts extends Product {
 
 interface ICartContext  {
     products: CartProduts[]
+    addProductToCart: (product: Product) => void
 }
 
 export const CartContext = createContext<ICartContext>({
-    products: []
+    products: [],
+    addProductToCart: () => {},
 })
+
+export const CartProvider = ({children}: {children: ReactNode}) =>{
+    const [products, setProducts] = useState<CartProduts[]>([])
+    
+    const addProductToCart = (product: Product) =>{
+        setProducts((prev) => [...prev, {...product, quantity: 0}])
+    }
+
+    return (
+        <CartContext.Provider value={{products, addProductToCart}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
