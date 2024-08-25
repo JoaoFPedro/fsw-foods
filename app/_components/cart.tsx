11aa410894a09f9bb11e94cb6130451dfe7d8b5d
Joao Pedro <joao.fpsilva98@gmail.com>
2024-08-25 14:06:59 -0300

    feat: adding formatCurrency method to prices

--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -2,24 +2,34 @@ import { useContext } from "react";
 import { CartContext } from "../_context/cart";
 import CartItem from "./cart-item";
 import { Card } from "./ui/card";
+import { formatCurrency } from "../_helpers/price";
 
 const Cart = () => {
-  const { products, subTotalPrice, totalPrice, totalDiscounts } = useContext(CartContext);
+  const { products, subTotalPrice, totalPrice, totalDiscounts } =
+    useContext(CartContext);
   return (
     <>
-    <div className="space-x-1 py-5">
-      {products.map((product) => (
-       <CartItem key={product.id}cartItem={product}/>
-      ))}
-    </div>
+      <div className="space-x-1 py-4">
+        {products.map((product) => (
+          <CartItem key={product.id} cartItem={product} />
+        ))}
+      </div>
 
-    <Card>
+      <Card className="p-5">
+        <div className="flex items-center justify-between border-b pb-2">
+          <span className="text-sm text-muted-foreground">Subtotal</span>
+          <span>{formatCurrency(subTotalPrice)}</span>
+        </div>
+        <div className="flex items-center justify-between border-b pb-2 pt-2">
+          <span className="text-sm text-muted-foreground">Descontos</span>
+          <span>- {formatCurrency(totalDiscounts)}</span>
+        </div>
 
-<h1>{subTotalPrice}</h1>
-<h1>{totalPrice}</h1>
-<h1>{totalDiscounts}</h1>
-</Card>
-
+        <div className="flex justify-between pb-2 pt-2">
+          <h1 className="font-semibold">Total</h1>
+          <span>{formatCurrency(totalPrice)}</span>
+        </div>
+      </Card>
     </>
   );
 };
