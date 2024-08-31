210acbb3cdfd5c14d329bcc7023d73d5af7c1ebf
Joao Pedro <joao.fpsilva98@gmail.com>
2024-08-31 12:21:23 -0300

    .

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,9 +5,8 @@ import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    adapter: PrismaAdapter(db) as Adapter,
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
-   
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
